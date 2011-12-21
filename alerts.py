9aee713ce3a8734c5a3303247ed6c4bf0cfdf912
Neil Williams <neil@reddit.com>
2011-12-21 00:25:56 -0800

    Add monitor that checks for queueing in HAProxy pools.

--- alerts.py
+++ alerts.py
@@ -4,14 +4,14 @@ import ConfigParser
 
 import wessex
 
-__all__ = ['harold']
+__all__ = ["harold", "config"]
 
 # load the configuration
 config = ConfigParser.RawConfigParser()
 config.read(['production.ini'])
-HAROLD_HOST = config.get('alerts', 'host')
-HAROLD_PORT = config.getint('alerts', 'port')
-HAROLD_SECRET = config.get('alerts', 'secret')
+HAROLD_HOST = config.get('harold', 'host')
+HAROLD_PORT = config.getint('harold', 'port')
+HAROLD_SECRET = config.get('harold', 'secret')
 
 # create the harold object
 harold = wessex.Harold(host=HAROLD_HOST, port=HAROLD_PORT, secret=HAROLD_SECRET)
---
+++ haproxy.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+import csv
+import urllib2
+import collections
+import time
+
+import alerts
+
+
+TIMEOUT = 3  # seconds
+CONFIG_SECTION = "haproxy"
+
+
+def fetch_queue_lengths_by_pool(haproxy_stats_urls):
+    pools = collections.Counter()
+
+    for url in haproxy_stats_urls:
+        csv_data = urllib2.urlopen(url, timeout=TIMEOUT)
+        reader = csv.reader(csv_data)
+
+        reader.next()  # skip the header
+        for row in reader:
+            proxy_name, server_name, queue_length = row[:3]
+            if server_name != "BACKEND":
+                continue
+            pools[proxy_name] += int(queue_length)
+
+    return pools
+
+
+def watch_request_queues(haproxy_urls, threshold, check_interval):
+    queued_pools = set()
+
+    while True:
+        # this *should* die if unable to talk to haproxy, then we'll
+        # get heartbeat-failure alerts
+        pools = fetch_queue_lengths_by_pool(haproxy_urls)
+
+        # alert where necessary
+        for pool, queue_length in pools.iteritems():
+            if queue_length > threshold:
+                if pool in queued_pools:
+                    alerts.harold.alert("queueing-%s" % pool,
+                                        "%s pool is queueing (%d)" %
+                                        (pool, queue_length))
+                queued_pools.add(pool)
+            else:
+                if pool in queued_pools:
+                    queued_pools.remove(pool)
+
+        # check in then sleep 'til we're needed again
+        alerts.harold.heartbeat("monitor_haproxy", check_interval * 2)
+        time.sleep(check_interval)
+
+
+def main():
+    # expects a config section like the following
+    # [haproxy]
+    # threshold = 200
+    # interval = 30
+    # url.* = url
+    haproxy_urls = [value for key, value in
+                    alerts.config.items(CONFIG_SECTION)
+                    if key.startswith("url")]
+    threshold = alerts.config.getint(CONFIG_SECTION, "threshold")
+    interval = alerts.config.getint(CONFIG_SECTION, "interval")
+
+    watch_request_queues(haproxy_urls, threshold, interval)
+
+
+if __name__ == "__main__":
+    main()
