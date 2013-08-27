95d615b2bdd0f12e93bc0624bcf52b65c63388c5
Neil Williams <neil@reddit.com>
2013-08-27 12:25:06 -0700

    utilization: Report haproxy queuing by pool to Graphite.

--- utilization.py
+++ utilization.py
@@ -13,6 +13,7 @@ import alerts
 def fetch_session_counts(haproxy_stats_urls):
     current = collections.Counter()
     limit = collections.Counter()
+    queue = {}
 
     for url in haproxy_stats_urls:
         csv_data = urllib2.urlopen(url, timeout=3)
@@ -21,7 +22,10 @@ def fetch_session_counts(haproxy_stats_urls):
         for i, row in enumerate(reader):
             if i == 0: continue
 
-            proxy_name, service_name, s_cur, s_lim, status = row[0], row[1], row[4], row[6], row[17]
+            proxy_name, service_name, q_cur, s_cur, s_lim, status = row[0], row[1], row[2], row[4], row[6], row[17]
+
+            if service_name == "BACKEND":
+                queue[proxy_name] = int(q_cur)
 
             if service_name in ("FRONTEND", "BACKEND"):
                 continue
@@ -32,24 +36,25 @@ def fetch_session_counts(haproxy_stats_urls):
             current[proxy_name] += int(s_cur)
             limit[proxy_name] += int(s_lim)
 
-    return [(pool, current[pool], capacity)
+    return [(pool, current[pool], capacity, queue[pool])
             for pool, capacity in limit.most_common()]
 
 
 def notify_graphite(usage):
     values = {}
-    for pool, cur, limit in usage:
+    for pool, cur, limit, queue in usage:
         values["stats.utilization.%s.current" % pool] = cur
         values["stats.utilization.%s.capacity" % pool] = limit
+        values["stats.utilization.%s.queue" % pool] = queue
     alerts.graphite.send_values(values)
 
 
 def pretty_print(usage):
     print "%20s%20s%10s" % ("", "sessions", "")
-    print "%20s%10s%10s%10s" % ("pool", "cur", "max", "% util")
-    print "-" * 50
-    for pool, cur, limit in usage:
-        print "%20s%10d%10d%10.2f" % (pool, cur, limit, cur / limit * 100.0)
+    print "%20s%10s%10s%10s%10s" % ("pool", "cur", "max", "% util", "queue")
+    print "-" * 60
+    for pool, cur, limit, queue in usage:
+        print "%20s%10d%10d%10.2f%10d" % (pool, cur, limit, cur / limit * 100.0, queue)
 
 
 def main():
