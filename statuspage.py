d7f50f92e8536a6e5628bad0801c19d20d92eb07
Neil Williams <neil@reddit.com>
2015-11-22 19:32:52 -0800

    statuspage: Update for Requests > 1.0

--- statuspage.py
+++ statuspage.py
@@ -42,7 +42,7 @@ class Graphite(object):
         response.raise_for_status()
 
         result = {}
-        for target_data in response.json:
+        for target_data in response.json():
             result[target_data["target"]] = target_data["datapoints"]
         return result
 
